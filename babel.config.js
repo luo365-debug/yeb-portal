0887dc550e746d2e015ba390d08d09295bffec95
luo365-debug <245718656@qq.com>
2022-01-15 22:43:44 +0800

    vue项目基本框架

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+//扩大浏览器支持
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+//程序入口
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//关闭浏览器控制台对环境的提示
 Vue.config.productionTip = false
 
+//实例化
 new Vue({
+  //路由
   router,
+  //用于渲染app的组件——>template style标签
   render: h => h(App)
+  //  手动挂载
 }).$mount('#app')
