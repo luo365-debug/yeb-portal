a4245f05f7d441cb17c9bd4182bbdd0eb56b2cbb
luo365-debug <245718656@qq.com>
2022-01-18 11:57:01 +0800

    登录事件

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <el-form ref="form" :model="loginForm" class="loginContainer">
+    <!-- ref表单名 -->
+    <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
       <h3 class="loginTitle">系统登陆</h3>
 
-      <el-form-item>
+      <!-- prop使其与校验规则rules里的名字对应 -->
+      <el-form-item prop="username">
         <!--输入框-->
         <el-input type="text" auto-complete="false" v-model="loginForm.username" aria-placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
         <el-input type="password" auto-complete="false" v-model="loginForm.password" aria-placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item prop="code">
         <el-input type="text" auto-complete="false" v-model="loginForm.code" aria-placeholder="点击图片更换验证码" style="width: 250px"></el-input>
         <img :src="captchaUrl" alt="验证码显示错误">
       </el-form-item>
 
       <el-checkbox v-model="checked" class="loginRemember">记住账号密码</el-checkbox>
-      <el-button type="primary" style="width: 100%">登录</el-button>
+      <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
 
     </el-form>
   </div>
@@ -32,7 +38,25 @@
           code:''
         },
         //默认勾选记住密码
-        checked:true
+        checked:true,
+        //校验不为空
+        rules:{
+          username: [{required:true,message:'请输入用户名',trigger:'blur'}],
+          password: [{required:true,message:'请输入密码',trigger:'blur'}],
+          code: [{required:true,message:'请输入验证码',trigger:'blur'}]
+        }
+      }
+    },
+    methods:{
+      submitLogin(){
+        this.$refs["loginForm"].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            this.$message.error("请将用户名、密码、验证码输入完整");
+            return false;
+          }
+        });
       }
     }
   }
@@ -46,7 +70,7 @@
     width: 350px;
     padding: 15px 35px 15px 35px;
     background-color: antiquewhite;
-    border: black 1px solid;
+    border: slategray 1px solid;
     box-shadow: 0 0 25px #cac6c6;
   }
 
